9239aeca97256a15a36da603dc2513b3b5c60024
evandesantola <edesanto@andrew.cmu.edu>
2016-09-11 05:47:55 -0400

    This is as good as research

--- freeLiveKans.py
+++ freeLiveKans.py
@@ -11,8 +11,14 @@ import time
 import json
 import uuid
 import urllib2
-
-
+import urllib
+import base64
+import getopt
+import MultipartPostHandler
+import os
+import re
+import sys
+from AbbyyOnlineSdk import *
 import skfuzzy
 import hmmlearn
 
@@ -26,6 +32,63 @@ fgbg = cv2.BackgroundSubtractorMOG()
 
 counter=0
 threshold=0
+
+
+
+
+processor = AbbyyOnlineSdk()
+
+if "ABBYY_APPID" in os.environ:
+	processor.ApplicationId = os.environ["ABBYY_APPID"]
+
+if "ABBYY_PWD" in os.environ:
+	processor.Password = os.environ["ABBYY_PWD"]
+
+# Proxy settings
+if "http_proxy" in os.environ:
+	proxyString = os.environ["http_proxy"]
+	print "Using proxy at %s" % proxyString
+	processor.Proxy = urllib2.ProxyHandler( { "http" : proxyString })
+
+
+
+# Recognize a file at filePath and save result to resultFilePath
+def recognizeFile( filePath, resultFilePath, language, outputFormat ):
+	print "Uploading.."
+	settings = ProcessingSettings()
+	settings.Language = language
+	settings.OutputFormat = outputFormat
+	task = processor.ProcessImage( filePath, settings )
+	if task == None:
+		print "Error"
+		return
+	print "Id = %s" % task.Id
+	print "Status = %s" % task.Status
+
+	# Wait for the task to be completed
+	sys.stdout.write( "Waiting.." )
+	# Note: it's recommended that your application waits at least 2 seconds
+	# before making the first getTaskStatus request and also between such requests
+	# for the same task. Making requests more often will not improve your
+	# application performance.
+	# Note: if your application queues several files and waits for them
+	# it's recommended that you use listFinishedTasks instead (which is described
+	# at http://ocrsdk.com/documentation/apireference/listFinishedTasks/).
+
+	while task.IsActive() == True :
+		time.sleep( 5 )
+		sys.stdout.write( "." )
+		task = processor.GetTaskStatus( task )
+
+	print "Status = %s" % task.Status
+	
+	if task.Status == "Completed":
+		if task.DownloadUrl != None:
+			processor.DownloadResult( task, resultFilePath )
+			print "Result was written to %s" % resultFilePath
+	else:
+		print "Error processing task"
+
 
 class Sticky(object):
 	def __init__(self, color, shape, isHeader, data, midpoint):
@@ -56,6 +119,7 @@ stickies  = []
 headers = []
 
 counter=0
+
 
 
 
@@ -143,6 +207,13 @@ while(1):
 				cv2.putText(image, text, (cX, cY),
 					cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 				newSticky= Sticky(str(color), str(shape), -1, c, (cX, cY))
+
+				x, y, width, height = cv2.boundingRect(c)
+				roi = frame[y:y+height, x:x+width]
+				cv2.imwrite("roi.png", roi)
+				recognizeFile( "roi.png", "out.txt", 'English', 'txt')
+				with open("out.txt", "r") as f:
+					newSticky.text=f.read().replace('\n', '')
 				stickies.append(newSticky)
 
 
@@ -219,12 +290,12 @@ while(1):
 			outputJson.append(currentList)
 
 		outputJson= json.dumps(outputJson)
-		req = urllib2.Request("http://de66c8cc.ngrok.io/api/kanban")
-		req.add_header('Content-Type', 'application/json')
+		#req = urllib2.Request("http://de66c8cc.ngrok.io/api/kanban")
+		#req.add_header('Content-Type', 'application/json')
 
 		print outputJson
 		response = urllib2.urlopen(req, outputJson)
-
+	
 
 
 
